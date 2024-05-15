420deb7f6c0f34f2d0f1808b0c91c8c90a1f787b
aviral shukla <aviralshukla@Mani.local>
2024-05-15 11:30:01 +0530

    Init to win it

---
+++ proxy/consumer/consumer.go
@@ -0,0 +1,37 @@
+package proxy_consumer
+
+import (
+	"log"
+	"time"
+
+	proxy_database "github.com/panpito/youtube/design_pattern/proxy/database"
+)
+
+type Consumer struct {
+}
+
+func (consumer Consumer) Process(database proxy_database.IDatabase) {
+	log.Print("init")
+
+	database.Set(proxy_database.Input{
+		Id:      "1",
+		Content: "good afternoon!",
+	})
+
+	log.Print(database.Get("1"))
+
+	log.Print(database.Get("1"))
+
+	database.Set(proxy_database.Input{
+		Id:      "1",
+		Content: "good evening!",
+	})
+
+	log.Print(database.Get("1"))
+
+	time.Sleep(2 * time.Second)
+	log.Print(database.Get("1"))
+
+	time.Sleep(2 * time.Second)
+	log.Print(database.Get("1"))
+}
---
+++ proxy/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	proxy_consumer "github.com/panpito/youtube/design_pattern/proxy/consumer"
+	proxy_database "github.com/panpito/youtube/design_pattern/proxy/database"
+)
+
+func main() {
+	consumer := proxy_consumer.Consumer{}
+	
+	db := proxy_database.NewDatabase("../design_pattern/proxy/db.json")
+	cache := proxy_database.NewCache(*db)
+
+	consumer.Process(cache)
+}
